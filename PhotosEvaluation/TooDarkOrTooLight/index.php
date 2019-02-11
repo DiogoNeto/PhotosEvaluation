59fe5dc10e68e0c2cafe8f15faf9d79b080be0e0
Diogo Neto <diogoncmc@gmail.com>
2019-02-11 11:36:23 +0000

    TooDarkOrTooLight

---
+++ PhotosEvaluation/TooDarkOrTooLight/index.php
@@ -0,0 +1,108 @@
+<?php
+// Start the session
+session_start();
+?>
+<!Doctype html>
+<html>
+<head>
+<title>Photos</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+  #container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+#div1,
+#div2 {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+#div1 {
+  z-index: 10;
+}
+#btnHelp {
+	position:absolute;
+	bottom:5px;
+	right: 5px;
+}
+
+</style>
+<script type="text/javascript">
+
+	$(document).ready(function(){
+  		$("#btnHelp").click(function(){
+    		$("#div1").fadeToggle(1000);
+  		});
+  	});
+
+		$(document).ready(function(){
+  		$("#btnStart").click(function(){
+  			//$_SESSION['btn1'] = "-1";
+    		$("#div3").fadeToggle(1000);
+    		$("#btnStart").fadeToggle(1000);
+  		});
+  	});
+</script>
+</head>
+<body>
+	<?php echo " User: "; echo $_SESSION['User']; echo " Session: "; echo $_SESSION['Sessao']; ?>
+	<h3 align="center">Muito escuro ou muito claro?</h3>
+<div id="container">
+	<!--Ajuda-->
+	<div id="div1" style="display:none;" style="background-color: #F0F0F0;" width="100%" height="100%">
+		<table class="table" width="100%" height="500px;">
+			<tr>
+				<td align="center" width="50%"><div align="right" ><img style="border:5px solid #ff0000; height: 500px;" src="../images/img_34.jpg"></div></td>
+				<td align="center" width="50%"><div align="left"><img style="border:5px solid #00ff00; height: 500px;" src="../images/TooDark.jpg"></div></td>
+			<tr>
+		</table>
+		<table align="center" width="100%">
+			<tr>
+				<td><p align="center">Nestas imagens deverá avaliar se o nível de luminosidade se encontra demasiado escuro ou demasiado claro. Caso se encontre com uma luminosidade adequada, clique em não.</p></td>
+			</tr>
+		</table>
+	</div>
+	
+	<!--Interação-->
+	<div id="div2" align="center">
+		<form action="index.php" method="POST" id="form1" name="form1" style="visibility: visible;">
+			<?php require_once '../ClosedEyes/process.php'; ?>
+			<?php $_SESSION['btni']='1' ?>
+			<!--<div id="div3" style="display: block; position: absolute; left:43%; top:0px; width:150px; height:100px; background-color:#FFFFFF; z-index:$_SESSION['btn1'];">
+				<button id="btnStart" class="btn btn-primary" id="btnStart" type="submit" name="not">Iniciar</button>
+			</div>-->
+			<table>
+				<tr>
+					<td><input type="text" style="visibility:hidden; max-height: 500px;" name="i" id="i" value="<?php echo $i; ?>"></td>
+				</tr>
+			</table>
+			<br/>
+			<div style="z-index:-2; ">
+				<table id="tbl1" width="100px" style="visibility:visible;">
+				<tr>
+					<td style="padding: 5px; z-index:-1;">
+						<button id="btnNao" class="btn btn-danger" type="submit" name="not">Não</button>
+					</td>
+					<td style="padding: 5px;">
+						<button id="btnSim" class="btn btn-success" type="submit" name="next">Sim</button>
+					</td>
+				</tr>
+			</table>
+			</div>
+		</form>
+	</div>
+	</div>
+	<!--Botão de Ajuda-->
+	<button id="btnHelp" class="btn btn-primary">Ajuda</button><br><br>
+</body>
+</html>
+
